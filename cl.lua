3425e8eb69569713d84413b53d01f1b4fe0a4b67
Vitali Baumtrok <vbsw@mailbox.org>
2025-07-23 15:01:40 +0200

    cl

---
+++ cl.lua
@@ -0,0 +1,249 @@
+--[[
+            Copyright 2025, Vitali Baumtrok.
+   Distributed under the Boost Software License, Version 1.0.
+       (See accompanying file LICENSE or copy at
+          http://www.boost.org/LICENSE_1_0.txt)
+]]
+
+local function extractSearchParams(paramsN, ...)
+	local searchTerms, searchTermsN, delimiter = nil, 0, nil
+	for i = 1, paramsN do
+		local param = select(i, ...)
+		local paramType = type(param)
+		if paramType == "table" then
+			if param:available() then
+				delimiter = param
+			end
+		elseif paramType == "string" then
+			searchTermsN = searchTermsN+1
+			if searchTerms == nil then
+				searchTerms = {param}
+			else
+				searchTerms[searchTermsN] = param
+			end
+		end
+	end
+	return searchTerms, searchTermsN, delimiter
+end
+
+local function matchByPrefix(argument, argumentN, searchTerm, searchTermN, delimiter)
+	local delimiterN, hasEmpty = delimiter.n, delimiter.hasEmpty
+	if (delimiterN > 0 or hasEmpty) and string.sub(argument, 1, searchTermN) == searchTerm then
+		for i = 1, delimiterN do
+			local dlmtr = delimiter[i]
+			local dlmtrN = #dlmtr
+			local searchTermWithDlmtrN = searchTermN+dlmtrN
+			if argumentN >= searchTermWithDlmtrN then
+				local dlmtrInArgument = string.sub(argument, searchTermN+1, searchTermWithDlmtrN)
+				if dlmtrInArgument == dlmtr then
+					return string.sub(argument, searchTermWithDlmtrN+1, argumentN)
+				end
+			end
+		end
+		if hasEmpty then
+			return string.sub(argument, searchTermN+1)
+		end
+	end
+	return nil
+end
+
+local function searchPairsWithSpace(cmdLine, args, searchTerms, searchTermsN, delimiter)
+	local arguments, matched, argumentsN = cmdLine.arguments, cmdLine.matched, cmdLine.n
+	local keys, values, argsN = args.keys, args.values, args.n
+	local i, allMatched = 1, true
+	while i <= argumentsN do
+		if not matched[i] then
+			local argument = arguments[i]
+			local argumentN = #argument
+			for j = 1, searchTermsN do
+				local searchTerm = searchTerms[j]
+				local searchTermN = #searchTerm
+				if argumentN == searchTermN then
+					if argument == searchTerm then
+						local value, iNxt = "", i+1
+						matched[i] = true
+						if iNxt <= argumentsN then
+							value = arguments[iNxt]
+							matched[iNxt] = true
+							i = iNxt
+						end
+						argsN = argsN+1
+						keys[argsN], values[argsN] = argument, value
+						break
+					end
+				elseif argumentN > searchTermN then
+					local value = matchByPrefix(argument, argumentN, searchTerm, searchTermN, delimiter)
+					if value then
+						matched[i] = true
+						argsN = argsN+1
+						keys[argsN], values[argsN] = searchTerm, value
+						break
+					end
+				end
+			end
+			allMatched = allMatched and matched[i]
+		end
+		i = i+1
+	end
+	matched[argumentsN+1] = allMatched
+	args.n = argsN
+end
+
+local function searchPairsWithoutSpace(cmdLine, args, searchTerms, searchTermsN, delimiter)
+	local arguments, matched, argumentsN, hasEmptyDelimiter = cmdLine.arguments, cmdLine.matched, cmdLine.n, delimiter.hasEmpty
+	local keys, values, argsN = args.keys, args.values, args.n
+	local i, allMatched = 1, true
+	for i = 1, argumentsN do
+		if not matched[i] then
+			local argument = arguments[i]
+			local argumentN = #argument
+			for j = 1, searchTermsN do
+				local searchTerm = searchTerms[j]
+				local searchTermN = #searchTerm
+				if argumentN == searchTermN then
+					if hasEmptyDelimiter and argument == searchTerm then
+						matched[i] = true
+						argsN = argsN+1
+						keys[argsN], values[argsN] = argument, ""
+						break
+					end
+				elseif argumentN > searchTermN then
+					local value = matchByPrefix(argument, argumentN, searchTerm, searchTermN, delimiter)
+					if value then
+						matched[i] = true
+						argsN = argsN+1
+						keys[argsN], values[argsN] = searchTerm, value
+						break
+					end
+				end
+			end
+			allMatched = allMatched and matched[i]
+		end
+	end
+	matched[argumentsN+1] = allMatched
+	args.n = argsN
+end
+
+local function search(cmdLine, ...)
+	local args = {keys = {}, values = {}, n = 0}
+	local matched, argumentsN = cmdLine.matched, cmdLine.n
+	if not matched[argumentsN+1] then
+		local paramsN = select("#", ...)
+		if argumentsN > 0 and paramsN > 0 then
+			local searchTerms, searchTermsN, delimiter = extractSearchParams(paramsN, ...)
+			if searchTermsN > 0 then
+				local arguments, allMatched = cmdLine.arguments, true
+				if delimiter == nil then
+					local keys, argsN = args.keys, 0
+					for i = 1, argumentsN do
+						if not matched[i] then
+							for j = 1, searchTermsN do
+								local argument = arguments[i]
+								if argument == searchTerms[j] then
+									local argsNNxt = argsN+1
+									keys[argsNNxt], argsN, matched[i] = argument, argsNNxt, true
+									break
+								end
+							end
+							allMatched = allMatched and matched[i]
+						end
+					end
+					matched[argumentsN+1] = allMatched
+					args.n = argsN
+				else
+					if delimiter.hasSpace then
+						searchPairsWithSpace(cmdLine, args, searchTerms, searchTermsN, delimiter)
+					else
+						searchPairsWithoutSpace(cmdLine, args, searchTerms, searchTermsN, delimiter)
+					end
+				end
+			end
+		end
+	end
+	return args
+end
+
+local function unmatched(cmdLine)
+	local args, argsN = {}, 0
+	local arguments, matched, argumentsN = cmdLine.arguments, cmdLine.matched, cmdLine.n
+	if not matched[argumentsN+1] then
+		for i = 1, argumentsN do
+			if not matched[i] then
+				argsN = argsN+1
+				args[argsN] = arguments[i]
+			end
+		end
+	end
+	args.n = argsN
+	return args
+end
+
+local function newCmdLine(args, ...)
+	local from, to
+	local cmdLine = {arguments = {}, matched = {}, n = 0}
+	local optsN = select("#", ...)
+	if optsN > 0 then
+		local opt1 = select(1, ...)
+		if type(opt1) == "number" then
+			if opt1 > 0 then
+				from = opt1
+			else
+				from = 1
+			end
+		else
+			error("2nd parameter not a number")
+		end
+		if optsN > 1 then
+			local opt2 = select(2, ...)
+			if type(opt2) == "number" then
+				if opt2 > 0 then
+					to = opt2
+				else
+					to = 0
+				end
+			else
+				error("3nd parameter not a number")
+			end
+		else
+			to = #args
+		end
+	else
+		from, to = 1, #args
+	end
+	for i = from, to do
+		local indexNew = i-from+1
+		cmdLine.arguments[indexNew] = args[i]
+		cmdLine.matched[indexNew] = false
+	end
+	cmdLine.n = to-from+1
+	-- last value means all arguments are matched
+	cmdLine.matched[cmdLine.n+1] = (cmdLine.n == 0)
+	cmdLine.search = search
+	cmdLine.unmatched = unmatched
+	return cmdLine
+end
+
+local function available(delimiter)
+	return delimiter.n > 0 or delimiter.hasEmpty or delimiter.hasSpace
+end
+
+local function newDelimiter(...)
+	local delimiter, n, hasEmpty, hasSpace, paramsN = {}, 0, false, false, select("#", ...)
+	for i = 1, paramsN do
+		local value = select(i, ...)
+		if type(value) == "string" then
+			if value == " " then
+				hasSpace = true
+			elseif value == "" then
+				hasEmpty = true
+			else
+				n = n+1
+				delimiter[n] = value
+			end
+		end
+	end
+	delimiter.n, delimiter.hasEmpty, delimiter.hasSpace, delimiter.available = n, hasEmpty, hasSpace, available
+	return delimiter
+end
+
+return {newCmdLine = newCmdLine, newDelimiter = newDelimiter}
---
+++ unittest.lua
@@ -0,0 +1,157 @@
+--[[
+            Copyright 2025, Vitali Baumtrok.
+   Distributed under the Boost Software License, Version 1.0.
+       (See accompanying file LICENSE or copy at
+          http://www.boost.org/LICENSE_1_0.txt)
+]]
+
+local cl = require("cl")
+
+local function asserType(obj, expectedType)
+	local objType = type(obj)
+	if objType ~= expectedType then
+		error("is type \""..objType.."\" (instead of \""..expectedType"\")", 2)
+	end
+end
+
+local function assertMemberType(obj, expectedKeys, expectedValueTypes)
+	for i = 1, #expectedKeys do
+		local expectedKey, foundValue, found = expectedKeys[i], nil, false
+		for key, value in pairs(obj) do
+			if key == expectedKey then
+				foundValue, found = value, true
+				break
+			end
+		end
+		if found then
+			local foundValueType, expectedValueType = type(foundValue), expectedValueTypes[i]
+			if foundValueType ~= expectedValueType then
+				error("key \""..expectedKey.."\" is type \""..foundValueType.."\" (instead of \""..expectedValueType"\")", 2)
+			end
+		else
+			error("is key \""..expectedKey.."\" missing (instead of available)", 2)
+		end
+	end
+end
+
+local function assertGr(value, expectedValue)
+	if value <= expectedValue then
+		local valueType = type(value)
+		if valueType == "string" then
+			local is = "is \""..value.."\" <= \""..expectedValue.."\""
+			local insteadOf = " (instead of \""..value.."\" > \""..expectedValue.."\")"
+			error(has..insteadOf, 2)
+		else
+			local is = "is "..value.." <= "..expectedValue
+			local insteadOf = " (instead of "..value.." > "..expectedValue..")"
+			error(has..insteadOf, 2)
+		end
+	end
+end
+
+local function assertEq(value, expectedValue)
+	if value ~= expectedValue then
+		local valueType = type(value)
+		if valueType == "string" then
+			error("is \""..value.."\" (instead of \""..expectedValue.."\")", 2)
+		else
+			error("is "..tostring(value).." (instead of "..tostring(expectedValue)..")", 2)
+		end
+	end
+end
+
+local function assertArryEq(value, expectedValues)
+	for i, val in ipairs(value) do
+		local expectedValue = expectedValues[i]
+		local valType, expectedValueType = type(val), type(expectedValue)
+		if valType ~= expectedValueType then
+			error("index "..i.." is type \""..valType.."\" (instead of \""..expectedValueType"\")", 2)
+		elseif val ~= expectedValue then
+			if valType == "string" then
+				error("index "..i.." is \""..val.."\" (instead of \""..expectedValue.."\")", 2)
+			else
+				error("index "..i.." is "..tostring(val).." (instead of "..tostring(expectedValue)..")", 2)
+			end
+		end
+	end
+end
+
+function testCL()
+	local cmdLine = cl.newCmdLine({"asdf", "--version"})
+	asserType(cmdLine, "table")
+	assertMemberType(cmdLine, {"arguments", "matched", "n"}, {"table", "table", "number"})
+	assertEq(#cmdLine.arguments, 2)
+	assertEq(#cmdLine.matched, 3)
+	assertEq(cmdLine.n, 2)
+	assertArryEq(cmdLine.matched, {false, false, false})
+	local argument = cmdLine:search("-v", "--version")
+	asserType(argument, "table")
+	assertMemberType(argument, {"keys", "values", "n"}, {"table", "table", "number"})
+	assertEq(#argument.keys, 1)
+	assertEq(#argument.values, 0)
+	assertEq(argument.n, 1)
+	assertEq(argument.keys[1], cmdLine.arguments[2])
+	assertArryEq(cmdLine.matched, {false, true, false})
+
+	cmdLine = cl.newCmdLine({"--start", "asdf", "-s", "qwer"})
+	assertEq(cmdLine.n, 4)
+	argument = cmdLine:search("-s", "--start")
+	assertEq(argument.n, 2)
+	assertEq(argument.keys[1], cmdLine.arguments[1])
+	assertEq(argument.keys[2], cmdLine.arguments[3])
+	assertArryEq(cmdLine.matched, {true, false, true, false, false})
+
+	cmdLine = cl.newCmdLine({"asdf", "--start=123"})
+	assertEq(cmdLine.n, 2)
+	local delimiter = cl.newDelimiter("=", "")
+	asserType(delimiter, "table")
+	assertMemberType(delimiter, {"hasEmpty", "hasSpace", "n"}, {"boolean", "boolean", "number"})
+	assertEq(#delimiter, 1)
+	assertEq(delimiter[1], "=")
+	assertEq(delimiter.n, 1)
+	assertEq(delimiter.hasEmpty, true)
+	assertEq(delimiter.hasSpace, false)
+	assertEq(delimiter:available(), true)
+	argument = cmdLine:search(delimiter, "-s", "--start")
+	assertEq(argument.n, 1)
+	assertEq(argument.values[1], "123")
+	assertArryEq(cmdLine.matched, {false, true, false})
+
+	cmdLine = cl.newCmdLine({"asdf", "--start", "123"})
+	assertEq(cmdLine.n, 3)
+	delimiter = cl.newDelimiter(" ", "=")
+	assertEq(#delimiter, 1)
+	assertEq(delimiter[1], "=")
+	assertEq(delimiter.n, 1)
+	assertEq(delimiter.hasEmpty, false)
+	assertEq(delimiter.hasSpace, true)
+	assertEq(delimiter:available(), true)
+	argument = cmdLine:search(delimiter, "-s", "--start")
+	assertEq(argument.n, 1)
+	assertEq(argument.values[1], "123")
+	assertArryEq(cmdLine.matched, {false, true, true, false})
+
+	cmdLine = cl.newCmdLine({"asdf", "--start123"})
+	assertEq(cmdLine.n, 2)
+	delimiter = cl.newDelimiter("=", "")
+	assertEq(delimiter:available(), true)
+	argument = cmdLine:search(delimiter, "-s", "--start")
+	assertEq(argument.n, 1)
+	assertEq(argument.values[1], "123")
+	assertArryEq(cmdLine.matched, {false, true, false})
+
+	cmdLine = cl.newCmdLine({"--start", "asdf", "-s", "qwer"})
+	assertEq(cmdLine.n, 4)
+	argument = cmdLine:search("--start", "-s")
+	assertArryEq(cmdLine.matched, {true, false, true, false, false})
+	local unmatched = cmdLine:unmatched()
+	assertEq(unmatched.n, 2)
+	assertEq(unmatched[1], cmdLine.arguments[2])
+	assertEq(unmatched[2], cmdLine.arguments[4])
+	cmdLine:search("asdf", "qwer")
+	assertArryEq(cmdLine.matched, {true, true, true, true, true})
+end
+
+function testLib()
+	testCL()
+end
